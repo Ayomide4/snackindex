c8b19b360ac7f740aa1091282f7419bc1be56338
Ayomide Omotosho <aomotosho4@gmail.com>
2025-08-20 15:03:52 -0500

    first commit

---
+++ collector/main.py
@@ -0,0 +1,63 @@
+# orchestration / entry point
+from datetime import datetime
+import logging
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+
+
+def connect_db():
+    try:
+        connection = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            dbname=os.getenv("DB_NAME"),
+        )
+        print("Connection to supabase db successful")
+        return connection
+    except psycopg2.Error as e:
+        print(f"Error connecting to supabase db: {e}")
+        return None
+
+
+def close_db_connection(connection):
+    if connection:
+        connection.close()
+        print("Database connection closed.")
+
+
+def fetch_data(connection, table_name):
+    if not connection:
+        print("No database connection available.")
+        return []
+
+    try:
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT * FROM {table_name}")
+        rows = cursor.fetchall()
+        column_names = [desc[0] for desc in cursor.description]
+        cursor.close()
+        return column_names, rows
+
+    except psycopg2.Error as e:
+        print(f"Error fetching data: {e}")
+        return [], []
+
+
+if __name__ == "__main__":
+    conn = connect_db()
+
+    if conn:
+        print("\n--- Fectching Data ---")
+        column_names, rows = fetch_data(conn, "snacks")
+        print(column_names, rows)
+        close_db_connection(conn)
