ee718fabfa84fe4b4c51ca07aa251994ab61b7c4
Ayomide Omotosho <aomotosho4@gmail.com>
2026-03-04 22:26:45 -0600

    update cors obj

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,19 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
     origin: [
       'https://snackindex-frontend.vercel.app',
-      'http://localhost:3000'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      'http://localhost:3000'
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
-
-/*
- * TODO:
- * - return daily metrics with snack names, date, article/reddit count, article/reddit urls?, search metrics, news/reddit sentiment, stock info
- *
- */
