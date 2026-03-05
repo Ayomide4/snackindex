7e96b1c9427ec8841d09539984787284c12fc82e
Ayomide Omotosho <aomotosho4@gmail.com>
2026-03-04 22:14:29 -0600

    enable cors for frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors({
+    origin: [
+      'https://snackindex-frontend.vercel.app',
+      'http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
